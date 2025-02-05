935d2b116a626cfbfe7b7148ee19b465efbf298e
dvankevich <dvankevich@gmail.com>
2025-02-05 22:17:12 +0200

    add database connection

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Fastify Getting-Started examples
 
 from https://fastify.dev/docs/latest/Guides/Getting-Started/
+
+## get mongoDB collections names
+
+`mongo test_database --quiet --eval "db.getCollectionNames()"
+`
+where `test_database` is database name
--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import Fastify from 'fastify';
 import firstRoute from './our-first-route.js';
+import dbConnector from './our-db-connector.js';
 
 // Inside of Fastify object you can write configuration for app
 const fastify = Fastify({
   logger: true, // Enable logger
 });
 
+fastify.register(dbConnector);
 fastify.register(firstRoute);
-
-fastify.get('/about', async function handler(request, reply) {
-  return { info: 'Super puper information is saved here' };
-});
 
 // Run web server
 try {
